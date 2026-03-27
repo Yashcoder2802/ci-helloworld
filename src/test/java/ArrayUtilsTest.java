5245ab71690bd605456706261397b39e501a2419
YashCoder2802 <omyash.ys@gmail.com>
2026-03-27 11:38:37 -0400

    Restore full ArrayUtils and tests

--- src/test/java/ArrayUtilsTest.java
+++ src/test/java/ArrayUtilsTest.java
@@ -1,8 +1,43 @@
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class ArrayUtilsTest {
+
+    @Test
+    void testFindLastNull() {
+        assertThrows(NullPointerException.class, () -> ArrayUtils.findLast(null, 3));
+    }
+
+    @Test
+    void testFindLastEmpty() {
+        assertEquals(-1, ArrayUtils.findLast(new int[]{}, 3));
+    }
+
+    @Test
+    void testFindLastNonExistent() {
+        assertEquals(-1, ArrayUtils.findLast(new int[]{1, 2}, 3));
+    }
+
+    @Test
+    void testFindLastExistsFirstElement() {
+        assertEquals(0, ArrayUtils.findLast(new int[]{2, 3, 5}, 2));
+    }
+
+    @Test
+    void testOddOrPosNull() {
+        assertThrows(NullPointerException.class, () -> ArrayUtils.oddOrPos(null));
+    }
+
+    @Test
+    void testOddOrPosAllPositives() {
+        assertEquals(3, ArrayUtils.oddOrPos(new int[]{1, 2, 3}));
+    }
+
+    @Test
+    void testOddOrPositiveBothPositivesAndNegatives() {
+        assertEquals(3, ArrayUtils.oddOrPos(new int[]{-3, -2, 0, 1, 4}));
+    }
 
     @Test
     void countOfReturnsZeroWhenEmpty() {
