80b4d5a4295d23ec86df6105e36519bc01b0efc1
YashCoder2802 <omyash.ys@gmail.com>
2026-03-27 12:41:17 -0400

    Clean up Task 3 repo structure

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ci-helloworld
+
+Task 3 deliverables are in `task3/task3.md`.
--- task3/ci-helloworld/src/main/java/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        int[] sample = {1, 2, 2, 3, 4, 2};
-        int target = 2;
-        int occurrences = ArrayUtils.countOf(sample, target);
-        System.out.println("Hello CI/CD. Count of " + target + " is " + occurrences);
-    }
-}
